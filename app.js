de3847f3ffde7061518b781a95c6250526dfd9b5
Rashad Hamilton <rhamil12@lab.cpcc.edu>
2019-03-09 12:53:17 -0500

    March 9th class

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/*myName = "Alberto"; */
+let myNumber ;
+myNumber
+let myString = "Rashad Hamilton";
+let myBoolean = true;
+let myObject = null;
+//let myUndefined = symbol;
+let myName;
+
+//String
+let myFirstName = "Rashad";
+let myLastName = "Hamilton";
+let myFullName ="${myFirstName} ${myLastName}";
+let fullName = "${myFirstName} ${myLastName}";
+let fullName1 = "First Name: ${myFirstName} ${myLastName}";
+
+
+let valueA =10;
+let valueB = "5";
+let total = valueA * valueB;
+
+/*(if (myName) {
+    var myName = "Rashad";
+} */
+
+
+
+console.log(fullName); 
+console.log(total);
+
